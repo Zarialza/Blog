85dcf8723fad27e7a8dc1c2df3c1c97acb9da770
SAJAN SHRESTHA <sajanbscsit@prime.edu.np>
2024-04-01 16:54:01 +0545

    styling - active links on the nav bar will have a active class which will give them a blue color and an underline

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelectorAll(".header_nav a"); // Select all nav links
+  const currentUrl = window.location.href; // Get current URL
 
+  navLinks.forEach((link) => {
+    console.log(link.href);
+    console.log(currentUrl);
+
+    if (link.href === currentUrl) {
+      link.classList.add("active"); // Add 'active' class if URLs match
+    }
+  });
+});
